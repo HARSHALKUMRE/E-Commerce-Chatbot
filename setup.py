16f08bda27d8c1b5a521c856cfc377dc00b0495f
HARSHALKUMRE <kumreharshalkumar@gmail.com>
2024-05-20 14:10:24 +0530

    setup & requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Ecommercebot",
+    version="0.0.1",
+    author="Harshal Kumre",
+    author_email="kumreharshalkumar@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
